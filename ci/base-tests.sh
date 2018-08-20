43d372c0298257278a2bffb665eead9ba56724af
Manish Goregaokar <manishsmail@gmail.com>
2018-08-20 10:58:49 -0700

    try force installing

--- ci/base-tests.sh
+++ ci/base-tests.sh
@@ -11,14 +11,15 @@ mkdir -p ~/rust/cargo/bin
 cp target/debug/cargo-clippy ~/rust/cargo/bin/cargo-clippy
 cp target/debug/clippy-driver ~/rust/cargo/bin/clippy-driver
 rm ~/.cargo/bin/cargo-clippy
+cargo install --force --path .
 # run clippy on its own codebase...
-PATH=$PATH:~/rust/cargo/bin cargo clippy --all-targets --all-features -- -D clippy
+cargo clippy --all-targets --all-features -- -D clippy
 # ... and some test directories
-cd clippy_workspace_tests && PATH=$PATH:~/rust/cargo/bin cargo clippy -- -D clippy && cd ..
-cd clippy_workspace_tests/src && PATH=$PATH:~/rust/cargo/bin cargo clippy -- -D clippy && cd ../..
-cd clippy_workspace_tests/subcrate && PATH=$PATH:~/rust/cargo/bin cargo clippy -- -D clippy && cd ../..
-cd clippy_workspace_tests/subcrate/src && PATH=$PATH:~/rust/cargo/bin cargo clippy -- -D clippy && cd ../../..
+cd clippy_workspace_tests && cargo clippy -- -D clippy && cd ..
+cd clippy_workspace_tests/src && cargo clippy -- -D clippy && cd ../..
+cd clippy_workspace_tests/subcrate && cargo clippy -- -D clippy && cd ../..
+cd clippy_workspace_tests/subcrate/src && cargo clippy -- -D clippy && cd ../../..
 # test --manifest-path
-PATH=$PATH:~/rust/cargo/bin cargo clippy --manifest-path=clippy_workspace_tests/Cargo.toml -- -D clippy
-cd clippy_workspace_tests/subcrate && PATH=$PATH:~/rust/cargo/bin cargo clippy --manifest-path=../Cargo.toml -- -D clippy && cd ../..
+cargo clippy --manifest-path=clippy_workspace_tests/Cargo.toml -- -D clippy
+cd clippy_workspace_tests/subcrate && cargo clippy --manifest-path=../Cargo.toml -- -D clippy && cd ../..
 set +x
